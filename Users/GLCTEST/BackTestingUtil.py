668d4a3d7c3e93d0507ab557bfeb6c9d09ab05b9
Linchen Gong <Linchen Gong>
2016-08-15 22:10:01 +0800

    update data reading

--- Users/GLCTEST/BackTestingUtil.py
+++ Users/GLCTEST/BackTestingUtil.py
@@ -19,54 +19,68 @@ def loadFundInfo(fundInfoFile):
     aRatio     = aRatio/sumab
     bRatio     = bRatio/sumab
     newdf      = pandas.concat([baseTicker, aTicker, bTicker, aRatio, bRatio], axis=1)
-    newdf.columns = ['baseTicker', 'aTicker', 'bTicker', 'aWeight', 'bWeight']
-    newdf.set_index(['baseTicker'], inplace=True)
+    newdf.columns = ['Ticker', 'ATicker', 'BTicker', 'AWeight', 'BWeight']
+    newdf.index   = newdf['Ticker']
     return newdf
 
 def getFundTickerData(ticker, fundDataFolder, simStart, simEnd):
-    fileName = fundDataFolder + '/' + ticker + '_PRICE.csv'
-    if ticker.startswith('16'):
-        fileName = fundDataFolder + '/' + ticker + '_VALUE.csv'
-    dffull = pandas.read_csv(fileName, sep=',')
-    dffull.set_index('Date', inplace=True)
-    dffull = dffull.reindex(index=dffull.index[::-1]) # for descending data, ascending should be better
-    dffull.index = dffull.index.map(lambda x: datetime.datetime(*(time.strptime(x, "(datetime.datetime(%Y, %m, %d, 0, 0),)")[0:6])))
-    sttid  = bisect.bisect_left(dffull.index, simStart)
-    endid  = bisect.bisect_left(dffull.index, simEnd)
-    if ticker.startswith('16'):
-        return dffull['UnitValue'][sttid:endid]
-    else:
-        return dffull['Close'][sttid:endid]
+    dummyS  = pandas.Series([0., 0.], index=[simStart, simEnd])
+    pxFile  = fundDataFolder + '/' + ticker + '_PRICE.csv'
+    valFile = fundDataFolder + '/' + ticker + '_VALUE.csv'
     
+    pxdf       = pandas.read_csv(pxFile, sep=',')
+    pxdf.set_index('Date', inplace=True)
+    #pxdf       = pxdf.reindex(index=pxdf.index[::-1]) # for descending data, ascending should be better
+    pxdf.index = pxdf.index.map(lambda x: datetime.datetime(*(time.strptime(x, "(datetime.datetime(%Y, %m, %d, 0, 0),)")[0:6])))
+    pxsttid    = bisect.bisect_left(pxdf.index, simStart)
+    pxendid    = bisect.bisect_left(pxdf.index, simEnd)
+
+    valdf       = pandas.read_csv(valFile, sep=',')
+    valdf.set_index('Date', inplace=True)
+    #valdf       = valdf.reindex(index=valdf.index[::-1]) # for descending data, ascending should be better
+    valdf.index = valdf.index.map(lambda x: datetime.datetime(*(time.strptime(x, "(datetime.datetime(%Y, %m, %d, 0, 0),)")[0:6])))
+    valsttid    = bisect.bisect_left(valdf.index, simStart)
+    valendid    = bisect.bisect_left(valdf.index, simEnd)
+    
+    sv = pandas.Series()
+    sp = pandas.Series()
+    if ticker.startswith('16'):
+        sv = valdf['UnitValue'][valsttid:valendid]
+        sp = pxdf['UnitValue'][pxsttid:pxendid]
+    else:
+        sv = valdf['UnitValue'][valsttid:valendid]
+        sp = pxdf['Close'][pxsttid:pxendid]
+    if len(sv) == 0:
+        sv = dummyS
+    if len(sp) == 0:
+        sp = dummyS
+    return (sv, sp)
+
 def loadFundData(fundDataFolder, fundInfo, simStart, simEnd):
     seriesList = []
-    headerList = []
+    keyList    = []
+    headerList = ['FundValue', 'AValue', 'BValue', 'FundPrice', 'APrice', 'BPrice']
+    
     for ticker in fundInfo.index:
-        aticker = fundInfo.ix[ticker]['aTicker']
-        bticker = fundInfo.ix[ticker]['bTicker']
-        tseries = getFundTickerData(ticker, fundDataFolder, simStart, simEnd)
-        aseries = getFundTickerData(aticker, fundDataFolder, simStart, simEnd)
-        bseries = getFundTickerData(bticker, fundDataFolder, simStart, simEnd)
-        if len(tseries) == 0:
-            tseries = pandas.Series([0., 0.], index=[simStart, simEnd])
-        if len(aseries) == 0:
-            aseries = pandas.Series([0., 0.], index=[simStart, simEnd])
-        if len(bseries) == 0:
-            bseries = pandas.Series([0., 0.], index=[simStart, simEnd])
-        seriesList += [tseries, aseries, bseries]
-        headerList += [ticker, aticker, bticker]
+        aticker = fundInfo.ix[ticker]['ATicker']
+        bticker = fundInfo.ix[ticker]['BTicker']
+        fv, fp  = getFundTickerData(ticker, fundDataFolder, simStart, simEnd)
+        av, ap  = getFundTickerData(aticker, fundDataFolder, simStart, simEnd)
+        bv, bp  = getFundTickerData(bticker, fundDataFolder, simStart, simEnd)
+        sloc    = [fv, av, bv, fp, ap, bp]
+        dfloc   = pandas.concat(sloc, axis=1)
+        dfloc.columns = headerList
+        seriesList.append(dfloc)
+        keyList.append(ticker)
         print "Done "+ticker
-    df = pandas.concat(seriesList, axis=1)
-    df.columns = headerList
+    df = pandas.concat(seriesList, axis=1, keys=keyList)
     df.fillna(0., inplace=True)
     foutname = fundDataFolder+'/dumpdata_'+simStart.strftime('%Y%m%d')+'_'+simEnd.strftime('%Y%m%d')+'.csv'
     df.to_csv(foutname, sep=',')
     return df
 
-
-
-
 class CashPos():
+    pass
 
 if __name__ == "__main__":
     try:
