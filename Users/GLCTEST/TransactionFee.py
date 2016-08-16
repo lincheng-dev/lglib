548825e35200cfea9c0cb8277d9c7514d2d96e78
Linchen Gong <Linchen Gong>
2016-08-16 22:01:34 +0800

    better organization

--- Users/GLCTEST/TransactionFee.py
+++ Users/GLCTEST/TransactionFee.py
@@ -20,7 +20,7 @@ class TxFee(object):
         
 class TxFeeFundExchTrade(TxFee): 
     __metaclass__ = TxSingleton
-    def __init__(self, txRate=0.0019):
+    def __init__(self, txRate=0.0003):
         super(TxFeeFundExchTrade, self).__init__(txRate)
 
 class TxFeeFundSub(TxFee):
